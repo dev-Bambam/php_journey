d29aa5bf99b75948967a7a61fab3c108b6d6a201
dev-Bambam <ayogood18@gmail.com>
2024-09-09 11:37:03 +0100

    now learning some nicks about MVC

--- index.php
+++ index.php
@@ -1,34 +1,3 @@
-<?php
-        $books = [
-            [
-                "title" => "The Great Gatsby",
-                "author" => "F. Scott Fitzgerald",
-                "year" => 1925
-            ],
-            [
-                "title" => "The Great BamBam",
-                "author" => "F. Scott Fitzgerald",
-                "year" => 2025
-            ],
-            [
-                "title" => "To Kill a Mockingbird",
-                "author" => "Harper Lee",
-                "year" => 1960
-            ],
-            [
-                "title" => "1984",
-                "author" => "George Orwell",
-                "year" => 1949
-            ],
-            [
-                "title" => "The Great Gods of Egypt 🫣",
-                "author" => "F. Scott Fitzgerald",
-                "year" => 1995
-            ],
-        ];
+<?php 
 
-        $filterAuthor = array_filter($books, function($book){
-            return $book["author"] == "F. Scott Fitzgerald";
-        });
-
-        require "index.view.php";
+require "views/index.view.php";   
