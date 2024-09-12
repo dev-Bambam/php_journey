7dcf185ee1537d11138489458dc5a00f73b69a7f
dev-Bambam <ayogood18@gmail.com>
2024-09-12 11:56:16 +0100

    learning PDO

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php 
 require 'functions.php';
 require 'router.php';
+
+// connect to database
+$dsn = 'mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4';
+$pdo = new PDO($dsn);
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+dumpDie($posts);
