7fc932d803875171aad52529fb7a8a368ae9c33f
dev-Bambam <ayogood18@gmail.com>
2024-10-02 13:05:41 +0100

    started using chatGPT to learn

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php 
-$name = $_REQUEST['fname'];
-echo "Hello " . $name . " Welcome to my website";
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // collect the form data 
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+
+    // simple output
+    echo "Hello, ". htmlspecialchars($name) . " your email is: " . htmlspecialchars($email);
+}
